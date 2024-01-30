9050b343a4a600f7d262c6a4efdc83c2ec4808a2
Jhon Fabio Cardona <programasjhonfa@gmail.com>
2024-01-30 16:14:26 -0500

    Project PHP docker-compose

---
+++ php/www/db.php
@@ -0,0 +1,43 @@
+<?php
+
+class Conexion
+{
+    public static $server = "mariadb_host"; 
+    public static $user = "root";
+    public static $password = "Custom.2020";
+    public static $db = "custom_db";
+
+
+
+    /* ===================== 
+	  CONEXION A LA DB CRM 
+	========================= */
+    static public function conectar()
+    {
+        $link = null;
+        try {
+            $server = self::$server;
+            $user = self::$user;
+            $password = self::$password;
+            $db = self::$db;
+
+            $link = new PDO(
+                "mysql:host=$server;port=3306;dbname=$db;charset=utf8mb4",
+                "$user",
+                $password,
+                array(
+                    PDO::ATTR_PERSISTENT => true, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                )
+            );
+        } catch (\Throwable $e) {
+            $link = "Ocurrio un error al intento de la conexión: " .  $e->getMessage(); 
+        } finally {
+            // $link = null;
+        }
+        return $link;
+    }
+
+}
+
+$conexion = Conexion::conectar();
+var_dump($conexion);
